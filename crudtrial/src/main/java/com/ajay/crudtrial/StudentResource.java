a97d07a79c9d3c014885670af719e3fd2e07a958
aj9mbf <52732743+aj9mbf@users.noreply.github.com>
2019-07-15 16:44:39 +0530

    update webhook trigger part2

--- crudtrial/src/main/java/com/ajay/crudtrial/StudentResource.java
+++ crudtrial/src/main/java/com/ajay/crudtrial/StudentResource.java
@@ -37,7 +37,7 @@ public class StudentResource {
 		try {
 			stdlist=StudentDao.getInstance().getStudent(rollno);
 			System.out.println("hi there");
-			Sysout.out.println("new way to trigger with webhook");
+			Sysout.out.println("new way to trigger with webhook part 2");
 		}
 		catch (Exception e) {
 		}
